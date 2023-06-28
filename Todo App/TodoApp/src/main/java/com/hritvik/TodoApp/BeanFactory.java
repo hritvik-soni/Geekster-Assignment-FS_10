56309177e7b2684295598a26e3dec40864b6690f
Hritvik Soni <hritviksoni768@gmail.com>
2023-06-28 10:00:29 +0530

    Added

---
+++ Todo App/TodoApp/src/main/java/com/hritvik/TodoApp/BeanFactory.java
@@ -0,0 +1,19 @@
+package com.hritvik.TodoApp;
+
+import com.hritvik.TodoApp.model.Todo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+@Configuration
+public class BeanFactory {
+
+    @Bean
+    public List<Todo> getDataSource()
+    {
+        return new ArrayList<>();
+    }
+}
