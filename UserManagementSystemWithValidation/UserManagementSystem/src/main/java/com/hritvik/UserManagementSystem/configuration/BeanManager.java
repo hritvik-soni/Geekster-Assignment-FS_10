c27f8699166021c9906ff266bd22116fd8f3737c
Hritvik Soni <hritviksoni768@gmail.com>
2023-06-28 09:58:37 +0530

    Added

---
+++ UserManagementSystemWithValidation/UserManagementSystem/src/main/java/com/hritvik/UserManagementSystem/configuration/BeanManager.java
@@ -0,0 +1,25 @@
+package com.hritvik.UserManagementSystem.configuration;
+
+import com.hritvik.UserManagementSystem.model.User;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+class BeanManager {
+
+    @Bean
+    public List<User> getInitializedList()
+    {
+        User initUser = new User(0,"demo","dmo@dmo.cpm","Atoz@123","9999999999",LocalDate.of(1996,12,12),19); //LocalDate.now(), LocalTime.now());
+        List<User> initList = new ArrayList<>();
+        initList.add(initUser);
+
+        return initList;
+    }
+}
+
