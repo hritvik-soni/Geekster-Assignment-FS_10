7eb02566ceadc238587044c0f29a0ea740f79b7d
Hritvik Soni <hritviksoni768@gmail.com>
2023-07-27 18:47:59 +0530

    Added

--- Blog_App_MCT_Test/Blogging-Platform-API/src/main/java/com/hritvik/BloggingPlatformAPI/model/enums/Gender.java
+++
@@ -1,5 +0,0 @@
-package com.hritvik.BloggingPlatformAPI.model.enums;
-
-public enum Gender {
-    MALE,FEMALE,OTHER
-}
--- Blog_App_MCT_Test/Blogging-Platform-API/src/main/java/com/hritvik/BloggingPlatformAPI/repository/IFollowRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hritvik.BloggingPlatformAPI.repository;
-
-import com.hritvik.BloggingPlatformAPI.model.Follow;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IFollowRepository extends JpaRepository<Follow,Long> {
-
-
-}
